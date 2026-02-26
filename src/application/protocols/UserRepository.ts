a82f0523f247db229c2e7a0373edbc9dcef23a80
ZippyBonhTM <zippybont@gmail.com>
2026-02-26 17:02:49 -0300

    feat(user): add findById method to UserRepository + add ShowProfileUseCase

--- src/application/protocols/UserRepository.ts
+++ src/application/protocols/UserRepository.ts
@@ -2,5 +2,6 @@ import User from "@/domain/User.js";
 
 export default interface UserRepository {
   findByEmail(email: string): Promise<User | null>;
+  findById(id: string): Promise<User | null>;
   save(user: User): Promise<void>;
 }
---
+++ src/application/usecases/ShowProfileUseCase.ts
@@ -0,0 +1,21 @@
+import type JwtService from '@/infrastructure/services/JwtService.js';
+import type { UserDTO } from '../dtos/LoginDTO.js';
+import toUserDTO from '../mappers/UserMapper.js';
+import type UserRepository from '../protocols/UserRepository.js';
+
+export default class ShowProfileUseCase {
+  constructor(
+    private userRepo: UserRepository,
+    private tokenService: JwtService
+  ) { }
+
+  async execute(userAccessToken: string): Promise<UserDTO | null> {
+    const accessPayload = this.tokenService.verify(userAccessToken);
+    if (!accessPayload) return null;
+
+    const user = await this.userRepo.findById(accessPayload.id!);
+
+    if (!user) return null;
+    return toUserDTO(user);
+  }
+}
